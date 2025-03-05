4a5a0014df980879409631c5b3a9b654ad152cb7
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2025-03-05 20:15:54 -0300

    fix: se corriguio la configuración y se comento

--- config/nvim/lua/plugins/autotag/init.lua
+++ config/nvim/lua/plugins/autotag/init.lua
@@ -1,4 +1,44 @@
 return {
-  'windwp/nvim-ts-autotag',
-  event = 'VeryLazy',
+  -- Automatically add closing tags for HTML and JSX
+  {
+    'windwp/nvim-ts-autotag',
+    event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
+    opts = {
+      filetypes = {
+        'html',
+        'templ',
+        'javascript',
+        'typescript',
+        'javascriptreact',
+        'typescriptreact',
+        'svelte',
+        'vue',
+        'tsx',
+        'jsx',
+        'rescript',
+        'xml',
+        'php',
+        'markdown',
+        'astro',
+        'glimmer',
+        'handlebars',
+        'hbs',
+      },
+    },
+    config = function(_, opts)
+      require('nvim-ts-autotag').setup(opts)
+    end,
+  },
+
+  -- Auto pairs
+  {
+    'echasnovski/mini.pairs',
+    event = 'VeryLazy',
+    opts = {},
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {}, -- this is equivalent to setup({}) function
+  },
 }
