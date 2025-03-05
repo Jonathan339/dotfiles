d5c2f24d24c65d3acbe2c0b5ae2c4432a1860d37
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2025-03-05 19:52:23 -0300

    chore: agrega relativenumber

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -4,6 +4,7 @@ vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 vim.opt.clipboard = 'unnamedplus'
+vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.title = true
 vim.opt.autoindent = true
