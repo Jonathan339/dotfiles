48c50997df2b50dd6c6ad66d51afbf896e24855e
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2025-04-26 18:24:09 -0300

    refactor: lsp refactor

--- config/nvim/lua/plugins/lsp/init.lua
+++ config/nvim/lua/plugins/lsp/init.lua
@@ -1,76 +1,78 @@
 local M = {
-  "williamboman/mason.nvim",
-  event = "VeryLazy",
-  cmd = { "Mason", "MasonInstall", "MasonUpdate" },
-  --lazy = false,
+  'williamboman/mason.nvim',
+  event = 'VeryLazy',
+  cmd = { 'Mason', 'MasonInstall', 'MasonUpdate' },
   dependencies = {
-    { "neovim/nvim-lspconfig", event = "BufReadPre" },
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "j-hui/fidget.nvim",
+    { 'neovim/nvim-lspconfig', event = 'BufReadPre' },
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'j-hui/fidget.nvim',
     {
-      "folke/neodev.nvim",
+      'folke/neodev.nvim',
       config = function()
-        require("neodev").setup({
+        require('neodev').setup({
           library = {
-            plugins = { "nvim-dap-ui" },
+            plugins = { 'nvim-dap-ui' },
             types = true,
           },
         })
       end,
     },
   },
-
-  config = function()
-    local ensure_installed = require("plugins.lsp.defaults").ensure_installed
-    local fidget = require("fidget")
-    local mason = require("mason")
-    local mason_installer = require("mason-tool-installer")
-    local mason_lspconfig = require("mason-lspconfig")
-
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
-
-    mason_installer.setup({
-      ensure_installed = ensure_installed,
-      -- auto_update = true, -- No actualizar automáticamente
-      -- run_on_start = true, -- Instalar herramientas al iniciar Neovim
-    })
-
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'MasonToolsUpdateCompleted',
-      callback = function()
-        vim.schedule(function()
-          vim.notify(
-            ' Mason-tool-installer has finished updating packages',
-            'info',
-            { title = 'Mason Tool Installer' }
-          )
-        end)
-      end,
-    })
-
-    mason_lspconfig.setup({
-      handlers = require("plugins.lsp.handlers"),
-    })
-
-    fidget.setup({
-      text = {
-        done = "✓", -- Ícono para indicar que el progreso está completo
-      },
-      window = {
-        relative = "win", -- La ventana de fidget se posiciona relativa a la ventana de Neovim
-      },
-      -- Si tienes configuraciones personalizadas de notificaciones, las puedes agregar aquí.
-    })
-  end,
 }
+
+M.config = function()
+  local ensure_installed = require('plugins.lsp.defaults').ensure_installed
+  M.mason()
+  M.mason_installer(ensure_installed)
+  M.mason_lspconfig()
+  M.fidget()
+end
+
+M.mason = function()
+  require('mason').setup({
+    ui = {
+      icons = {
+        package_installed = '✓',
+        package_pending = '➜',
+        package_uninstalled = '✗',
+      },
+    },
+  })
+end
+
+M.mason_installer = function(ensure_installed)
+  require('mason-tool-installer').setup({
+    ensure_installed = ensure_installed,
+    -- auto_update = true,
+    -- run_on_start = true,
+  })
+
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'MasonToolsUpdateCompleted',
+    callback = function()
+      vim.schedule(function()
+        vim.notify(' Mason-tool-installer has finished updating packages', 'info', { title = 'Mason Tool Installer' })
+      end)
+    end,
+  })
+end
+
+M.mason_lspconfig = function()
+  require('mason-lspconfig').setup({
+    handlers = require('plugins.lsp.handlers'),
+  })
+end
+
+M.fidget = function()
+  require('fidget').setup({
+    text = {
+      done = '✓',
+    },
+    window = {
+      relative = 'win',
+    },
+  })
+end
 
 return M
